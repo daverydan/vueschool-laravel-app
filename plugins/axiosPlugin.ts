ae8d020bac4ca37f44364a6b657351f912ff51d5
daverydan <danny.w.avery@gmail.com>
2023-03-11 12:33:25 -0500

    configure axios plugin to get csrf token

--- plugins/axiosPlugin.ts
+++ plugins/axiosPlugin.ts
@@ -1,1 +1,11 @@
-export default defineNuxtPlugin((nuxtApp) => {})
+import axios from "axios"
+
+export default defineNuxtPlugin(async (nuxtApp) => {
+	axios.defaults.baseURL = "http://localhost:84";
+	axios.defaults.headers.common['X-Requested-With'] = "XMLHttpRequest";
+	axios.defaults.headers.post['Content-Type'] = 'application/json';
+	axios.defaults.headers.post['Accept'] = 'application/json';
+	axios.defaults.withCredentials = true;
+
+	await axios.get("/sanctum/csrf-cookie")
+})
