e66a5d76883be8a41bbddbabb027b4b45738a074
activeDannyA <danny.avery@activelogic.io>
2023-03-14 23:18:29 -0400

    manually handle form validation

--- pages/login.vue
+++ pages/login.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { AxiosError } from "axios";
+
   definePageMeta({
     layout: "centered",
     middleware: ["guest"],
@@ -10,19 +12,40 @@
     email: "",
     password: "",
   })
+
+  const errors = ref({
+    email: [],
+    password: [],
+  })
+
+  const handleLogin = async () => {
+    try {
+      await login(form.value);
+    } catch (error) {
+      if (error instanceof AxiosError && error.response?.status === 422) {
+        errors.value = error.response.data.errors;
+      }
+    }
+  }
 </script>
 <template>
   <div class="login">
     <h1>Login</h1>
-    <form @submit.prevent="login(form)">
+    <form @submit.prevent="handleLogin">
       <label>
         <div>Email</div>
         <input v-model="form.email" type="text" />
+        <div v-for="error in errors.email" :key="error" class="text-red-600 p-1">
+          {{ error }}
+        </div>
       </label>
 
       <label>
         <div>Password</div>
         <input v-model="form.password" type="password" />
+        <div v-for="error in errors.password" :key="error" class="text-red-600 p-1">
+          {{ error }}
+        </div>
       </label>
       <button class="btn">Login</button>
     </form>
