a2f76b1eee60b17926ba2997a0e6fa945678951f
activeDannyA <danny.avery@activelogic.io>
2023-03-15 00:12:18 -0400

    +createLink() to pages/links/create

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,8 +12,9 @@ export default defineNuxtConfig({
       appURL: "http://localhost:84",
     },
   },
-  routeRules: {
-    "/profiles/*": { swr: true },
-    "/*": { ssr: false },
-  },
+  ssr: false,
+  // routeRules: {
+  //   "/profiles/*": { swr: true },
+  //   "/*": { ssr: false },
+  // },
 });
