adcbb222b15bcce4017c59a5dfe5a3f3d7dce522
daverydan <danny.w.avery@gmail.com>
2023-03-21 21:26:02 -0400

    25-sort-data-on-fe-w/-laravel-query-builder-from-spaite

--- components/TableTh.vue
+++ components/TableTh.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
-const props = defineProps({
-  name: { type: String, required: true },
-  modelValue: { type: String, required: true },
-});
+  const props = defineProps({
+    name: { type: String, required: true },
+    modelValue: { type: String, required: true },
+  });
 
-defineEmits<{
-  (e: "update:modelValue", payload: string): void;
-}>();
+  defineEmits<{
+    (e: "update:modelValue", payload: string): void;
+  }>();
 
-const withoutDash = computed(() => props.name.replace(/^-/, ""));
-const withDash = computed(() => `-${withoutDash.value}`);
+  const withoutDash = computed(() => props.name.replace(/^-/, ""));
+  const withDash = computed(() => `-${withoutDash.value}`);
 
-const sorting = computed(
-  () => props.name === props.modelValue.replace(/^-/, "")
-);
-const desc = computed(() => props.modelValue.startsWith("-"));
+  const sorting = computed(
+    () => props.name === props.modelValue.replace(/^-/, "")
+  );
+  const desc = computed(() => props.modelValue.startsWith("-"));
 </script>
+
 <template>
   <th>
     <button
@@ -24,10 +25,10 @@ const desc = computed(() => props.modelValue.startsWith("-"));
     >
       <slot></slot>
       <button v-if="sorting && desc">
-        <IconChevronDown class="inline w-2 ml-2" />
+        <IconChevronDown class="inline w-[10px] ml-2" />
       </button>
       <button v-if="sorting && !desc">
-        <IconChevronUp class="inline w-2 ml-2" />
+        <IconChevronUp class="inline w-[10px] ml-2" />
       </button>
     </button>
   </th>
